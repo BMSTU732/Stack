7c102d82ce3f2123fe396515c2522baa5c3c8a40
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-12-07 22:08:36 +0300

    Update push.cpp

--- tests/push.cpp
+++ tests/push.cpp
@@ -6,45 +6,45 @@ SCENARIO("push() must add value to the top of the stack")
 {
     GIVEN("Empty stack")
     {
-        stack<int> s;
+        stack<int> st;
         WHEN("Adding an element")
         {
-            s.push(0);
+            st.push(0);
             THEN("push() must allocate memory for 5 elements and add value 0 to the top of the stack")
             {
-                REQUIRE( ((s.size() == 5) && (s.count() == 1) && (s.pop() == 0)) == true );
+                REQUIRE( ((st.size() == 5) && (st.count() == 1) && (st.pop() == 0)) == true );
             }
         }
     }
     GIVEN("Filled stack")
     {
-        stack<int> s;
-        s.push(0);
-        s.push(1);
+        stack<int> st;
+        st.push(0);
+        st.push(1);
         WHEN("Adding an element")
         {
-            s.push(2);
+            st.push(2);
             THEN("Value 2 must be at the top of the stack and number of elements must be 3")
             {
-                REQUIRE( ((s.count() == 3) && (s.pop() == 2)) == true );
+                REQUIRE( ((st.count() == 3) && (st.pop() == 2)) == true );
             }
         }
     }
     GIVEN("Full stack")
     {
-        stack<int> s;
-        s.push(0);
-        size_t size = s.size();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
+        stack<int> st;
+        st.push(0);
+        size_t size = st.size();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
         WHEN("Adding an element")
         {
-            s.push(5);
+            st.push(5);
             THEN("push() must reallocate memory by doubling previous and add value 5 to the top of the stack")
             {
-                REQUIRE( ((s.size() == (size*2)) && (s.count() == 6) && (s.pop() == 5)) == true );
+                REQUIRE( ((st.size() == (size*2)) && (st.count() == 6) && (st.pop() == 5)) == true );
             }
         }
     }

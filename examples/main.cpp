68baac6f39c52f6a2b5e0683c279c2aebf629ee3
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-12-07 18:08:46 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,27 +3,29 @@
 
 int main()
 {
-    try
-    {
-        stack<int> st;
-        std::cout << st.count() << std::endl;
-        st.push(0);
+	try
+	{
+		stack<int> st;
+		std::cout << st.count() << std::endl;
+		st.push(0);
 
-        for (int i = 1; i < 6; ++i)
-        {
-            st.push(i);
-            std::cout << st.count() << std::endl
-        }
-        for (int i = 0; i < 6; ++i)
-        {
-            std::cout << "Poped: " << st.pop() << std::endl;
-            std::cout << "Count = " << st.count() << std::endl;
-        }
+		for (int i = 1; i < 6; ++i)
+		{
+			st.push(i);
+			std::cout << st.count() << std::endl;
+		}
+	
+			for (int i = 0; i < 6; ++i)
+			{
+				std::cout << "Poped: " << st.pop() << std::endl;
+				std::cout << "Count = " << st.count() << std::endl;
+			}
 
-    }
-    catch (std::underflow_error& error)
-    {
-        std::cout << error.what() << std::endl;
-    }
-    return 0;
+	}
+	catch (std::underflow_error& error)
+	{
+		std::cout << error.what() << std::endl;
+	}
+	system("pause");
+	return 0;
 }

ead3205c574d57cd2114e608cd18dd3d0d40c3fb
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-12-12 22:43:15 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,31 +1,23 @@
 #include <stack.hpp>
 #include <iostream>
+setlocale(LC_ALL, "Russian");
 
 int main()
 {
-	try
-	{
-		stack<int> st;
-		std::cout << st.count() << std::endl;
-		st.push(0);
-
-		for (int i = 1; i < 6; ++i)
-		{
-			st.push(i);
-			std::cout << st.count() << std::endl;
-		}
-	
-			for (int i = 0; i < 6; ++i)
-			{
-				std::cout << "Poped: " << st.pop() << std::endl;
-				std::cout << "Count = " << st.count() << std::endl;
-			}
-
-	}
-	catch (std::underflow_error& error)
-	{
-		std::cout << error.what() << std::endl;
-	}
-	system("pause");
-	return 0;
+    stack<int> st;
+    std::cout << st.empty() << std::endl;
+    for (int i = 0; i < 6; ++i)
+    {
+        std::cout << "Добавление: " << i << std::endl;
+        st.push(i);
+    }
+    std::cout << st.empty() << std::endl;
+    for (int i = 0; i < 6; ++i)
+    {
+        std::cout << "Удаление: " << st.top() << std::endl;
+        st.pop();
+        if (!st.empty()) std::cout << "Вершина Stack: " << st.top() << std::endl;
+    }
+    std::cout << st.empty() << std::endl;
+    return 0;
 }

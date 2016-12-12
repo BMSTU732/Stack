4249256dc7b750299b0eda945554e7c6e7290579
Roman Petropavlov <roma24.petropavlov@gmail.com>
2016-12-12 22:52:16 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,66 +1,112 @@
 #include <cstddef>
-#include <exception>
 #include <stdexcept>
-
+#include <algorithm>
 
 template <typename T>
 class stack
 {
 public:
-    stack() : array_(nullptr), array_size_(0), count_(0) {}
-    auto size() const -> size_t;
-    auto count() const -> size_t;
-    auto push(const T&) -> void;
-    auto pop() -> T;
-    ~stack() { delete[] array_; }
+	stack() : array_(new T[init_size_]), array_size_(init_size_), count_(0) {}
+	auto size() const noexcept->size_t;
+	auto count() const noexcept->size_t;
+	auto empty() const noexcept -> bool;
+	auto push(const T&) -> void;
+	auto pop() noexcept -> void;
+	auto top() const -> T;
+	~stack() { delete[] array_; }
 private:
-    T* array_;
-    size_t array_size_;
-    size_t count_;
+	static const size_t init_size_  5;
+	T* array_;
+	size_t array_size_;
+	size_t count_;
 };
 
 
 template <typename T>
-auto stack<T>::size() const -> size_t
+auto stack<T>::size() const noexcept -> size_t
 {
-    return array_size_;
+	return array_size_;
 }
 
 
 template <typename T>
-auto stack<T>::count() const -> size_t
+auto stack<T>::count() const noexcept -> size_t
 {
-    return count_;
+	return count_;
 }
 
 
 template <typename T>
-auto stack<T>::push(const T& value) -> void
+auto stack<T>::empty() const noexcept -> bool
 {
-    if (!array_size_)
-    {
-        array_ = new T [5];
-        array_size_ = 5;
-    }
-    if (count_ == array_size_)
-    {
-        T* new_array = new T [array_size_*2];
-        for (int i = 0; i < count_; ++i)
-            new_array[i] = array_[i];
-        delete[] array_;
-        array_ = new_array;
-        array_size_ *= 2;
-    }
-    array_[count_++] = value;
-    return;
+	return (count_ ? false : true);
 }
 
 
 template <typename T>
-auto stack<T>::pop() -> T
+auto stack<T>::push(const T& value) /*strong*/ -> void
 {
-    if (count_)
-        return array_[--count_];
-    else
-        throw std::underflow_error("Stack is empty!");
+	T* backup = array_;
+	T* new_array = nullptr;
+	bool realloc = false;
+	if (count_ == array_size_)
+	{
+		array_size_ *= 2;
+
+		try
+		{
+			new_array = new T[array_size_];
+			std::copy(array_, array_ + count_, new_array);
+		}
+		catch (...)
+		{
+			delete[] new_array;
+			array_size_ /= 2;
+			throw;
+		}
+
+		array_ = new_array;
+		realloc = true;
+	}
+
+	try
+	{
+		array_[count_] = value;
+	}
+	catch (...)
+	{
+		if (realloc)
+		{
+			delete[] array_;
+			array_ = backup;
+			array_size_ /= 2;
+		}
+		throw;
+	}
+
+	++count_;
+	if (realloc) delete[] backup;
+	return;
 }
+
+
+template <typename T>
+auto stack<T>::pop() noexcept -> void
+{
+	if (count_)
+		--count_;
+	return;
+}
+
+
+template <typename T>
+auto stack<T>::top() const /*strong*/ -> T
+{
+	if (count_)
+	{
+		T value = array_[count_ - 1];
+		return value;
+	}
+	throw std::underflow_error("Stack is empty!");
+}
+
